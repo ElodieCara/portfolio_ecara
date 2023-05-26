f6255b9025ac670dc6597102c85e1f87bd761dc0
ElodieCara <102527658+ElodieCara@users.noreply.github.com>
2023-05-26 15:05:23 +0200

    maj header

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,79 +1,3 @@
-// import { useState, useEffect, useRef } from "react";
-// import { useLocation } from "react-router-dom";
-// import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
-// import { HiOutlineMail } from "react-icons/hi";
-// import { BsFillPersonLinesFill } from "react-icons/bs";
-// import Logo from "../../assets/Logo.png";
-
-// const Navbar = () => {
-//   const [nav, setNav] = useState(false);
-//   const location = useLocation();
-//   const handleClick = () => setNav(!nav);
-
-//   useEffect(() => {
-//     // Fermer le menu mobile lorsque l'utilisateur navigue vers une autre page
-//     setNav(false);
-//   }, [location.pathname]);
-
-//   return (
-//     <nav className="nav container">
-//       <a href="#" className="nav__logo">
-//         <img src={Logo} alt="Logo" style={{ width: "180px" }} />
-//       </a>
-//       {/* menu */}
-//       <div className="nav__menu" id="nav-menu">
-//         <ul className="nav__list grid">
-//           <li>
-//             <a href="#home">Accueil</a>
-//           </li>
-//           <li className="nav__item">
-//             <a href="#skills" className="nav__link">
-//               Compétences
-//             </a>
-//           </li>
-//           <li className="nav__item">
-//             <a href="#portfolio" className="nav__link">
-//               Portfolio
-//             </a>
-//           </li>
-//           <li className="nav__item">
-//             <a href="#contact" className="nav__link">
-//               Contact
-//             </a>
-//           </li>
-//         </ul>
-//       </div>
-//       {/* Hamburger */}
-//       <div onClick={() => setNav(!nav)} className="nav__btns">
-//         {!nav ? <FaBars /> : <FaTimes />}
-//       </div>
-//       {/* Mobile menu */}
-//       <ul
-//         className="nav__mobile"
-//         style={!nav ? { visibility: "hidden" } : { width: "100%" }}
-//       >
-//         <li onClick={handleClick} className="py-6 text-4xl">
-//           <a href="#home">Home</a>
-//         </li>
-//         <li onClick={handleClick} className="py-6 text-4xl">
-//           <a href="#about">About</a>
-//         </li>
-//         <li onClick={handleClick} className="py-6 text-4xl">
-//           <a href="#skills">Skills</a>
-//         </li>
-//         <li onClick={handleClick} className="py-6 text-4xl">
-//           <a href="#work">Work</a>
-//         </li>
-//         <li onClick={handleClick} className="py-6 text-4xl">
-//           <a href="#contact">Contact</a>
-//         </li>
-//       </ul>
-//       {/* s */}
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/Logo.png";
@@ -85,7 +9,7 @@ function NavBar() {
   return (
     <>
       <nav className="navbar">
-        <div className="nav container">
+        <div className="nav">
           <NavLink exact to="/" className="nav__logo">
             <img src={Logo} alt="Logo" style={{ width: "80px" }} />
           </NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Header from "../components/Header/Header";
 import Navbar from "../components/NavBar/Navbar";
 
 export default function Home() {
   return (
     <header className="header" id="header">
       <Navbar />
+      <Header />
     </header>
   );
 }
