8113b92b67b463b29bdb7069c2f560e7eaf26029
ElodieCara <102527658+ElodieCara@users.noreply.github.com>
2023-07-09 22:56:13 +0200

    maj logos

--- src/data/data.js
+++ src/data/data.js
@@ -87,8 +87,8 @@ export const dataWorks = [
     date: "23/05/22",
     capture: BookiSite,
     techno: [
-      { url: "../images/logos/html-5.png", alt: "Html" },
-      { url: "../images/logos/css-3.png", alt: "CSS" },
+      { url: "/images/logos/html-5.png", alt: "Html" },
+      { url: "/images/logos/css-3.png", alt: "CSS" },
     ],
     link: [
       {
@@ -120,8 +120,8 @@ export const dataWorks = [
     date: "10/07/22",
     capture: OhmyfoodSite,
     techno: [
-      { url: "../images/logos/html-5.png", alt: "Html" },
-      { url: "../images/logos/sass.png", alt: "Sass" },
+      { url: "/images/logos/html-5.png", alt: "Html" },
+      { url: "/images/logos/sass.png", alt: "Sass" },
     ],
     link: [
       {
@@ -152,7 +152,7 @@ export const dataWorks = [
     libellé: "Optimiser un site web existant",
     date: "09/08/22",
     capture: PanthereSite,
-    techno: [{ url: "../images/logos/seo.svg", alt: "Seo" }],
+    techno: [{ url: "/images/logos/seo.svg", alt: "Seo" }],
     link: [
       {
         url: "https://elodiecara.github.io/P4-La_Panthere/",
@@ -178,9 +178,9 @@ export const dataWorks = [
     date: "25/10/22",
     capture: PiquanteSite,
     techno: [
-      { url: "../images/logos/express.svg", alt: "Express" },
-      { url: "../images/logos/node-js.png", alt: "NodeJS" },
-      { url: "../images/logos/mongodb-icon.png", alt: "MongoDB" },
+      { url: "/images/logos/express.svg", alt: "Express" },
+      { url: "/images/logos/node-js.png", alt: "NodeJS" },
+      { url: "/images/logos/mongodb-icon.png", alt: "MongoDB" },
     ],
     // link: [
     //   {
@@ -205,7 +205,7 @@ export const dataWorks = [
     libellé: "Construire un site e-commerce en JavaScript",
     date: "03/10/22",
     capture: KanapSite,
-    techno: [{ url: "../images/logos/js.png", alt: "JS" }],
+    techno: [{ url: "/images/logos/js.png", alt: "JS" }],
     // link: [
     //   {
     //     url: "none",
@@ -230,9 +230,9 @@ export const dataWorks = [
     date: "17/11/22",
     capture: KasaSite,
     techno: [
-      { url: "../images/logos/react.png", alt: "React" },
+      { url: "/images/logos/react.png", alt: "React" },
 
-      { url: "../images/logos/sass.png", alt: "Sass" },
+      { url: "/images/logos/sass.png", alt: "Sass" },
     ],
     link: [
       {
@@ -264,19 +264,19 @@ export const dataDesign = [
       "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
     date: "01/01/22",
     techno: [
-      { url: "../images/logos/react.png", alt: "React" },
+      { url: "/images/logos/react.png", alt: "React" },
 
-      { url: "../images/logos/sass.png", alt: "Sass" },
+      { url: "/images/logos/sass.png", alt: "Sass" },
     ],
     link: [
-      { icon: "../images/logos/link.svg", url: "", name: "Lien Chez Charlie" },
+      { icon: "/images/logos/link.svg", url: "", name: "Lien Chez Charlie" },
       {
-        icon: "../images/logos/github.svg",
+        icon: "/images/logos/github.svg",
         url: "https://github.com/ElodieCara/chez-charlie",
         name: "Code Chez Charlie",
       },
       {
-        icon: "../images/logos/figma.png",
+        icon: "/images/logos/figma.png",
         url: "https://www.figma.com/proto/e8oFLY0WbN62cjh2UBH0gr/Chez-Charlie?type=design&scaling=scale-down&page-id=0%3A1&node-id=1-4",
         name: "Prototype Chez Charlie",
       },
@@ -290,23 +290,23 @@ export const dataDesign = [
       "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
     date: "01/01/22",
     techno: [
-      { url: "../images/logos/react.png", alt: "React" },
-      { url: "../images/logos/vite.svg", alt: "Vite" },
-      { url: "../images/logos/sass.png", alt: "Sass" },
+      { url: "/images/logos/react.png", alt: "React" },
+      { url: "/images/logos/vite.svg", alt: "Vite" },
+      { url: "/images/logos/sass.png", alt: "Sass" },
     ],
     link: [
       {
-        icon: "../images/logos/link.svg",
+        icon: "/images/logos/link.svg",
         url: "none",
         name: "Lien vers le portfolio",
       },
       {
-        icon: "../images/logos/github.svg",
+        icon: "/images/logos/github.svg",
         url: "https://github.com/ElodieCara/portfolio_ecara",
         name: "Code du portfolio",
       },
       {
-        icon: "../images/logos/figma.png",
+        icon: "/images/logos/figma.png",
         url: "https://www.figma.com/proto/zpOh34P302zPQ9Ao4AyLRp/MYPORTFOLIO?type=design&node-id=20-2&scaling=min-zoom&page-id=0%3A1",
         name: "Prototype du portfolio",
       },
